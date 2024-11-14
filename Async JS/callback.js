9a89728f9acf9720a6d8864f21fcda78d2c083fe
aravindsanalkumar <aravindsanalkumar@gmail.com>
2024-11-14 11:48:21 +0530

    callback hell created

--- Async JS/callback.js
+++ Async JS/callback.js
@@ -13,8 +13,6 @@ function two(){
     console.log('step 2 took 2 sec')
 }
 
-
-
 // order from the customer
 // fetch the ingredients
 // start production
@@ -35,16 +33,36 @@ let stocks = {
  // select topping -3
  // serve ice cream -2
 
-let order = (call_back) => {
-    console.log('order placed , call production');
+let order = (fruit_name,call_backProduction) => {
+    console.log('order placed,call production');
     setTimeout(()=>{
-        call_back();
+        console.log(`${stocks.Fruits[fruit_name]} taken`);
+        call_backProduction();
     },2000)
 }
 
-
 let production = () => {
-    console.log('order received in 2 sec , starting production');
+    setTimeout(()=>{
+        console.log('production started');
+        setTimeout(()=>{
+            console.log('fruit has been chopped');
+            setTimeout(()=>{
+                console.log(`adding ${stocks.liquid[0]} and ${stocks.liquid[1]} `);
+                setTimeout(()=>{
+                    console.log('starting the machine');
+                    setTimeout(()=>{
+                        console.log(`${stocks.holder[1]} selected`);
+                        setTimeout(()=>{
+                            console.log(`${stocks.toppings[0]} selected as topping`);
+                            setTimeout(()=>{
+                                console.log('ice cream served');
+                            },2000)
+                        },3000)
+                    },2000)
+                },1000)
+            },1000)
+        },2000)
+    },0)
 }
 
-order(production);
+order(0,production);
